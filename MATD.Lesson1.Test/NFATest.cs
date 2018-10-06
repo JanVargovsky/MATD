2b129a1db98f8d00047e342844da25bce63de92f
Jan Vargovský <jan.vargovsky@gmail.com>
2018-10-06 20:31:31 +0200

    Lesson 1 - NFA + Test.

---
+++ MATD.Lesson1.Test/NFATest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace MATD.Lesson1.Test
+{
+    public class NFATest
+    {
+        [Fact]
+        public void EndingWith01Test()
+        {
+            var nfa = new NFA<char, int>(1, new[] { 3 },
+                (1, '0', new[] { 1, 2 }),
+                (1, '1', new[] { 1 }),
+                (2, '1', new[] { 3 }));
+
+            Assert.True(nfa.IsAccepted("01".ToCharArray()));
+            Assert.True(nfa.IsAccepted("001".ToCharArray()));
+            Assert.True(nfa.IsAccepted("101".ToCharArray()));
+            Assert.True(nfa.IsAccepted("10101010101".ToCharArray()));
+            Assert.True(nfa.IsAccepted("00000000001".ToCharArray()));
+
+            Assert.False(nfa.IsAccepted("".ToCharArray()));
+            Assert.False(nfa.IsAccepted("0".ToCharArray()));
+            Assert.False(nfa.IsAccepted("1".ToCharArray()));
+            Assert.False(nfa.IsAccepted("00".ToCharArray()));
+            Assert.False(nfa.IsAccepted("10".ToCharArray()));
+            Assert.False(nfa.IsAccepted("11".ToCharArray()));
+            Assert.False(nfa.IsAccepted("00000000000".ToCharArray()));
+            Assert.False(nfa.IsAccepted("11111111111".ToCharArray()));
+        }
+    }
+}
---
+++ MATD.Lesson1/NFA.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MATD.Lesson1
+{
+    public class NFA<TSymbol, TState>
+    {
+        readonly TState _startState;
+        readonly HashSet<TState> _endStates;
+        readonly Dictionary<(TState, TSymbol), IEnumerable<TState>> _transitionFunctions;
+
+        public NFA(TState start, IEnumerable<TState> end, params (TState state, TSymbol symbol, IEnumerable<TState> newStates)[] transitionFunctions)
+        {
+            _startState = start;
+            _endStates = end.ToHashSet();
+
+            _transitionFunctions = transitionFunctions.ToDictionary(t => (t.state, t.symbol), t => t.newStates);
+        }
+
+        public bool IsAccepted(params TSymbol[] input)
+        {
+            var currentStates = new HashSet<TState>() { _startState };
+
+            foreach (var symbol in input)
+            {
+                var newStates = new HashSet<TState>();
+
+                foreach (var currentState in currentStates)
+                {
+                    if (_transitionFunctions.TryGetValue((currentState, symbol), out var states))
+                    {
+                        newStates.UnionWith(states);
+                    }
+                }
+
+                currentStates = newStates;
+            }
+
+            return currentStates.Any(_endStates.Contains);
+        }
+    }
+}
