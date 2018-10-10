f7b795e4998352e61943232a001ec10a7220f811
Jan Vargovský <jan.vargovsky@gmail.com>
2018-10-10 23:15:47 +0200

    Lesson2 - benchmark added.

---
+++ MATD.Lesson2.Benchmark/Program.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Linq;
+
+namespace MATD.Lesson2.Benchmark
+{
+    public class PatternMatchingBenchmark
+    {
+        readonly string text;
+        readonly string pattern;
+
+        public PatternMatchingBenchmark()
+        {
+            text = new string('a', 100000) + "b";
+            pattern = new string('a', 50) + "b";
+        }
+
+        [Benchmark]
+        public int[] StringIndexOf() => text.AllIndexesOf(pattern).ToArray();
+
+        [Benchmark]
+        public int[] RegexMatches() => text.AllMatches(pattern).ToArray();
+
+        [Benchmark]
+        public int[] Naive() => text.Naive(pattern).ToArray();
+
+        [Benchmark]
+        public int[] BoyerMooreHorspool() => text.BoyerMooreHorspool(pattern).ToArray();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<PatternMatchingBenchmark>();
+        }
+    }
+}
