ac16f17f6e6de26b9c0353135386982044b01916
Jan Vargovský <jan.vargovsky@gmail.com>
2018-12-26 22:28:49 +0100

    Project - Added project that can export preprocessed documents.
    It exports all parsed documents in its raw form and then as preprocessed stems. Then it converts all documents into vectors and exports all documents(vectors) into one big csv file.

---
+++ MATD.Project/DocumentSaver.cs
@@ -0,0 +1,63 @@
+﻿using MATD.Lesson5;
+using MATD.Lesson8;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MATD.Project
+{
+    public class DocumentSaver
+    {
+        public async Task SaveAsync(string directory, IEnumerable<Document> documents)
+        {
+            foreach (var document in documents)
+            {
+                await SaveRawAsync(directory, document);
+                await SaveStemsAsync(directory, document);
+            }
+        }
+
+        async Task SaveRawAsync(string directory, Document document)
+        {
+            string fullPath = Path.GetFullPath(directory) + $"/{document.Id}.raw.txt";
+            using (var writer = new StreamWriter(fullPath))
+            {
+                await writer.WriteAsync(document.RawData);
+                await writer.FlushAsync();
+            }
+        }
+
+        async Task SaveStemsAsync(string directory, Document document)
+        {
+            string fullPath = Path.GetFullPath(directory) + $"/{document.Id}.stems.txt";
+            using (var writer = new StreamWriter(fullPath))
+            {
+                foreach (var item in document.Stems)
+                {
+                    await writer.WriteLineAsync(item);
+                }
+                await writer.FlushAsync();
+            }
+        }
+
+        public async Task SaveAsync(string directory, VectorDocumentCollection documents, int? count)
+        {
+            string fileName = !count.HasValue ? "vector-documents.csv" : $"vector-documents-{count}.csv";
+            string fullPath = Path.GetFullPath(directory) + '/' + fileName;
+            using (var writer = new StreamWriter(fullPath))
+            {
+                const char separator = ';';
+                string header = string.Join(separator, documents.Stems);
+                await writer.WriteLineAsync(header);
+
+                foreach (var document in documents.GetDocuments())
+                {
+                    string row = string.Join(separator, document.Values);
+                    await writer.WriteLineAsync(row);
+                }
+
+                await writer.FlushAsync();
+            }
+        }
+    }
+}
---
+++ MATD.Project/Program.cs
@@ -0,0 +1,52 @@
+﻿using MATD.Lesson5;
+using MATD.Lesson8;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MATD.Project
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var loader = new ReutersDataLoader();
+            var documents = new List<Document>();
+            Console.WriteLine("Loading documents.");
+            var files = Directory.GetFiles("Data/reuters21578/", "*.sgm");
+            foreach (var file in files)
+            {
+                documents.AddRange(await loader.ParseAsync(file));
+            }
+            Console.WriteLine("Loading documents done.");
+
+            Console.WriteLine("Preprocessing documents.");
+            var stemmer = new Stemmer();
+            var stopList = new StopList();
+            var textPreprocessing = new TextPreprocessing(stopList, stemmer);
+            textPreprocessing.RemoveInvalidCharacters(documents);
+            textPreprocessing.SplitToTokens(documents);
+            textPreprocessing.ApplyStopList(documents);
+            textPreprocessing.ApplyStemming(documents);
+            Console.WriteLine("Preprocessing documents done.");
+
+            const string directory = "Data/reuters21578-preprocessed";
+            var documentSaver = new DocumentSaver();
+
+            Console.WriteLine("Saving documents.");
+            await documentSaver.SaveAsync(directory, documents);
+            Console.WriteLine("Saving documents done.");
+
+            Console.WriteLine("Creating vector documents.");
+            var vectorDocuments = new VectorDocumentCollection(documents);
+            Console.WriteLine("Creating vector documents done.");
+
+            Console.WriteLine("Saving vector documents.");
+            await documentSaver.SaveAsync(directory, vectorDocuments, documents.Count);
+            Console.WriteLine("Saving vector documents done.");
+
+            Console.WriteLine("Done");
+        }
+    }
+}
